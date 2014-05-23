87d910c361858cc630a29758ce4030f5e2f74880
BJB2088 <BJB2088@gmail.com>
2014-05-23 14:08:41 -0700

    cmd scale, etc..

--- App-1/server.R
+++ App-1/server.R
@@ -1,3 +1,4 @@
+# Identify clusters in congressional voting records
 # Load PR3q First!!
 head(PR3q)
 # Dems = 1, Reps =2
@@ -8,10 +9,36 @@ for(i in 1:17){
   PR3q[,i]=as.numeric(PR3q[,i])
 }
 # K modes
-#cl <- kmodes(PR3q, 4)
+# Scree plot: 
+cl1 <- kmodes(PR3q, 1)
+cl2 <- kmodes(PR3q, 2)
+cl3 <- kmodes(PR3q, 3)
+cl4 <- kmodes(PR3q, 4)
+cl5 <- kmodes(PR3q, 5)
+cl6 <- kmodes(PR3q, 6)
+x=c(sum(cl1$with),sum(cl2$with),sum(cl3$with),sum(cl4$with),sum(cl5$with),sum(cl6$with))
+plot(1:6,x) 
+
 ## and visualize with some jitter:
-#plot(jitter(PR3q[,1]),jitter(PR3q[,2]), col = cl$cluster)
+#plot(jitter(PR3q[,1]),jitter(PR3q[,2]), col = cl$cluster,pch=cl$cluster)
 #points(cl$modes, col = 1:17, pch = 8)
+
+# Make dissimilaries matrix (SLOW)
+mat=matrix(rep(0,435*435),nrow=435,ncol=435)
+count=0
+for(i in 1:435){
+  print(i)
+  for(j in 1:435){
+    count = 0
+    for(k in 1:17){
+    count = count+ifelse(PR3q[i,k]==PR3q[j,k],0,1)
+    }
+    mat[i,j] = count
+}}
+
+# Multi dimensional scaling
+cmd=cmdscale(mat,k=2)
+plot(cmd[,1],cmd[,2],col=cl3$cluster)
 
 shinyServer(function(input, output) {
   #sel <- reactive(quote({input$checkGroup}),quoted=TRUE)
