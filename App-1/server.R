df05d80b36de12aa73c1f0d1c2814608c293e9bc
BJB2088 <BJB2088@gmail.com>
2014-05-23 12:48:57 -0700

    general udpates

--- App-1/server.R
+++ App-1/server.R
@@ -3,21 +3,23 @@ head(PR3q)
 # Dems = 1, Reps =2
 # ? = 1, no = 2, yes =3
 # Transform data to numeric
+table(PR3q[,1])
 for(i in 1:17){
   PR3q[,i]=as.numeric(PR3q[,i])
 }
 # K modes
-#(cl <- kmodes(PR3q, 2))
+#cl <- kmodes(PR3q, 4)
 ## and visualize with some jitter:
 #plot(jitter(PR3q[,1]),jitter(PR3q[,2]), col = cl$cluster)
 #points(cl$modes, col = 1:17, pch = 8)
 
 shinyServer(function(input, output) {
   #sel <- reactive(quote({input$checkGroup}),quoted=TRUE)
-  cl <- reactive(kmodes(PR3q, input$slider2))
+  cl <- reactive(kmodes(PR3q[,1:2], input$slider2))
   output$plt1 = renderPlot(plot(jitter(PR3q[,input$slider[1]]),jitter(PR3q[,input$slider[2]]), col = cl()$cluster,
                                 xlab=paste("Question",input$slider[1]),
                                 ylab=paste("Question",input$slider[2])))
+  
   })
 
 
--- App-1/ui.R
+++ App-1/ui.R
@@ -8,7 +8,9 @@ shinyUI(fluidPage(
                   min = 1, max = 5, value = 2),
       p("Which pair of variables?"),
       sliderInput("slider", "",
-                  min = 1, max = 17, value = c(1, 2))
+                  min = 1, max = 17, value = c(1, 2)),
+      br(),
+      p("q1:Dem(1) or Rep(2)")
     ),
     mainPanel(
       textOutput("txt1"),
